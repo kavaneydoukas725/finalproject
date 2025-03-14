3a320671caa82e16bca109cf03fe57c6609a3325
kavaneydoukas725 <kavaneydoukas725@gmail.com>
2025-03-14 16:17:30 +0000

    Add 9.java

---
+++ src/9.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class FinalProject {
+    public static void main(String[] args) {
+        // write your code here
+        int num = (int)(Math.random()*10);
+        System.out.println("The random number is: " + num);
+    }
+}
