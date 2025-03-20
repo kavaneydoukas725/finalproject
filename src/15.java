33ae55685353a09f5b741849bb23e61c332c46ee
kavaneydoukas725 <kavaneydoukas725@gmail.com>
2025-03-20 04:39:41 +0000

    Add 15.java

---
+++ src/15.java
@@ -0,0 +1,16 @@
+
+package com.finalproject;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // TODO: Write your code here
+        int i = 10;
+        double d = 23.5;
+        String s = "Hello, World!";
+        boolean b = true;
+        System.out.println("i = " + i);
+        System.out.println("d = " + d);
+        System.out.println("s = " + s);
+        System.out.println("b = " + b);
+    }
+}
