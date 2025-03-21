227c18fe7debb4b77f3bc071b11245c9488b0b98
kavaneydoukas725 <kavaneydoukas725@gmail.com>
2025-03-21 07:00:40 +0000

    Add 16.java

---
+++ src/16.java
@@ -0,0 +1,6 @@
+public class FinalProject {
+    public static void main(String[] args) {
+        // Code for the final project goes here
+        System.out.println("This is the final project code.");
+    }
+}
