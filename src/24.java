b48a2af7cdba0899d57794954ca38d43981848ac
kavaneydoukas725 <kavaneydoukas725@gmail.com>
2025-04-01 15:22:46 +0000

    Add 24.java

---
+++ src/24.java
@@ -0,0 +1,16 @@
+public class FinalProject {
+    public static void main(String[] args) {
+        // Code to be executed when the program starts
+        System.out.println("Starting Final Project...");
+        
+        // Additional code here
+        int i = 5;
+        double pi = Math.PI;
+        String str1 = "Hello, World!";
+        
+        // Code for demonstration purposes only
+        System.out.println(i);
+        System.out.println(pi);
+        System.out.println(str1);
+    }
+}
