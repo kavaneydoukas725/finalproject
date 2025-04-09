720d94eebf991cd4e4d620761fff5645bcbe7381
kavaneydoukas725 <kavaneydoukas725@gmail.com>
2025-04-09 14:53:00 +0000

    Add 30.java

---
+++ src/30.java
@@ -0,0 +1,17 @@
+public class FinalProject {
+    public static void main(String[] args) {
+        // Example 1: Print "Hello, World!"
+        System.out.println("Hello, World!");
+
+        // Example 2: Add two numbers
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        // Example 3: Use a for loop to print numbers from 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
